6c1862197dc9a40ede5160effe293fcee4c85265
Eka Putra <anonym415@gmail.com>
2016-02-12 20:58:56 +0700

    dot

--- .fluxbox/xfile/ciremai.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cat /home/x43u/.fluxbox/themes/ciremai > /home/x43u/.fluxbox/init
-cat /home/x43u/.fluxbox/themes/ciremai-apps > /home/x43u/.fluxbox/apps
-cat /home/x43u/.fluxbox/themes/ciremai-gtk2 > /home/x43u/.gtkrc-2.0
-cat /home/x43u/.fluxbox/themes/ciremai-gtk3 > /home/x43u/.config/gtk-3.0/settings.ini
-cat /home/x43u/.fluxbox/themes/ciremai-wallpaper > /home/x43u/.fehbg
--- .fluxbox/xfile/nela.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cat /home/x43u/.fluxbox/themes/nela > /home/x43u/.fluxbox/init
-cat /home/x43u/.fluxbox/themes/nela-apps > /home/x43u/.fluxbox/apps
-cat /home/x43u/.fluxbox/themes/nela-gtk2 > /home/x43u/.gtkrc-2.0
-cat /home/x43u/.fluxbox/themes/nela-gtk3 > /home/x43u/.config/gtk-3.0/settings.ini
-cat /home/x43u/.fluxbox/themes/nela-wallpaper > /home/x43u/.fehbg
